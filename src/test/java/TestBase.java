e7e2df86a1f28041495aa1e97dcb63416877f121
Maksim Pozniak <abc-1765@ya.ru>
2026-01-13 23:23:24 +0300

    registrationTest

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import pages.RegistrationPage;
+
+public class TestBase {
+
+    String firstName = "Alex",
+            LastName = "Egorov",
+            userEmail = "alex@egorov.com";
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+//        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+//        Configuration.holdBrowserOpen = true;
+    }
+
+}
---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,82 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationPage {
+
+    ///// SelenideElements
+    CalendarComponent calendar = new CalendarComponent();
+    SelenideElement titleLabel = $(".practice-form-wrapper"),
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            userNumberInput = $("#userNumber");
+
+
+    ///// Actions
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        titleLabel.shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+//    public void clickFirstName() { // not used
+//        firstNameInput.click();
+//    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setUserNumber(String value) {
+        userNumberInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirth(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        calendar.setDate("30", "July", "2008");
+
+        return this;
+    }
+
+
+    public RegistrationPage checkResult(String key, String value) {
+        $(".table-responsive").$(byText(key)).parent()
+                .shouldHave(text(value));
+
+        return this;
+    }
+
+}
